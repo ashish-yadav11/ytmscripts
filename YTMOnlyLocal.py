6e6647a42f6c761ce8399633f9176770942fd68f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-07-13 11:15:29 +0530

    Fixed typo

--- YTMOnlyLocal.py
+++ YTMOnlyLocal.py
@@ -6,7 +6,7 @@ import string
 
 #lkplylstid = "PL9cE5Kd6uzpgUN5jZDyX1RvU6wQRt4co3"
 unplylstid = "PL9cE5Kd6uzpiu0WpDfY5T4rexKsYoa4E7"
-#lkmusicdir = "/media/storage/Music"
+lkmusicdir = "/media/storage/Music"
 unmusicdir = "/media/storage/Music/archive"
 
 ytmusic = YTMusic("/home/ashish/.config/ytmusic-oauth.json")
