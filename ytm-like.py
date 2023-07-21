10b77b04014885e3dbcf77b23095c9540f5d30a8
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-07-21 19:49:06 +0530

    This was what i meant

--- ytm-like.py
+++ ytm-like.py
@@ -134,7 +134,7 @@ if not found:
     downloadfailed = False
     # download with yt-dlp
     ydlopts = {
-        'ignoreerrors': 'only_download',
+        'noprogress': True,
         'format': 'bestaudio/best',
         'outtmpl': {'default': '/media/storage/Music/%(title)s (%(id)s).%(ext)s'},
         'postprocessors': [{'key': 'FFmpegExtractAudio',
